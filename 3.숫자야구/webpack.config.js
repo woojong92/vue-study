a1ce57188189d1b1a0ada08e702c4a5f1e68f6fe
Woojong Kim <woojong92@day34.com>
2019-07-18 20:24:06 +0900

    숫자야구

---
+++ 3.숫자야구/webpack.config.js
@@ -0,0 +1,38 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const path = require('path');
+
+
+module.exports = {
+
+    mode : 'development',
+
+    devtool : 'eval', 
+
+    resolve : {
+        extensions : ['.js', '.vue'],
+    },
+
+
+    entry: {
+        app: path.join(__dirname, 'main.js'),  // app => 하나로 합쳐질 이름
+    },
+
+    module :  {
+        rules: [{
+            test: /\.vue$/, 
+            loader: 'vue-loader',
+        }],
+    },
+
+    plugins : [
+        new VueLoaderPlugin(),
+    ],
+
+    output : {
+        filename: '[name].js', // or app.js
+        path: path.join(__dirname, 'dist'), 
+    },
+};
+
+
+
